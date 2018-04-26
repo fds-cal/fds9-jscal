3a29f7521b17fac83ae2a27576bc84542860f323
Kim Sanghyeok <skyee23@gmail.com>
2018-04-26 16:43:59 +0900

    코드수정

--- calculator.js
+++ calculator.js
@@ -1,5 +1,7 @@
 // 아름다운 코드
 let display1 = document.querySelector(".display");
+let reset = document.querySelector('.op-ac');
+let change = document.querySelector('op-neg');
 let number = [
   document.querySelector(".num-0"),
   document.querySelector(".num-1"),
@@ -13,6 +15,15 @@ let number = [
   document.querySelector(".num-9"),
   document.querySelector(".num-dot")
 ];
+let operator = [
+  document.querySelector(".op-plus"),
+  document.querySelector(".op-minus"),
+  document.querySelector(".op-multi"),
+  document.querySelector(".op-divide"),
+  document.querySelector(".op-eql")
+]
+
+let countNumber = 0;
 
 //숫자 눌러주는것은 디스플레이에 보여주는 로직
 function displayOutput() {
@@ -22,13 +33,39 @@ function displayOutput() {
         if (index !== 10) {
           display1.textContent += index.toString();
           display1.textContent = parseFloat(display1.textContent).toString();
-        } else if (!display1.textContent.includes(".")) {
+          countNumber = parseFloat(display1.textContent);
+          console.log(countNumber);
+        } else if 
+        (!display1.textContent.includes(".")) {
           display1.textContent += ".";
         }
       }
     });
   });
+  resetDispaly();
 }
-displayOutput();
 
-// function
+function resetDispaly(){
+  reset.addEventListener('click',()=>{
+    display1.textContent = 0 ;
+  })
+}
+
+function operatorAll(){
+  operator.forEach((el,index)=>{
+    el.addEventListener('click', () =>{
+      if(el[index] === 0){
+        display1.textContent = countNumber;
+      }
+      else if(el[idex] === 1){
+
+      }
+
+
+    });
+  });
+}
+
+
+
+displayOutput();
